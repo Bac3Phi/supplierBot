588b2aa66fd72acb5243cbc2c46f07e24acc7993
Bac3Fi <gkztest1@gmail.com>
2020-01-12 12:00:28 +0700

    Init

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+require 'sidekiq/web'
+
+Rails.application.routes.draw do
+  resources :posts
+    authenticate :user, lambda { |u| u.admin? } do
+      mount Sidekiq::Web => '/sidekiq'
+    end
+
+
+  devise_for :users, controller: {
+    registrations: "registrations"
+  }
+  root to: 'posts#index'
+end
